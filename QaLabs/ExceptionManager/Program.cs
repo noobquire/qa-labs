33788fcb66cb62cf8ac31017f721667827a5fd07
Oleksii Lytvynov <Oleksii_Lytvynov@epam.com>
2021-02-03 10:58:53 +0200

    Project init

---
+++ QaLabs/ExceptionManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QaLabs/QaLabs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QaLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
