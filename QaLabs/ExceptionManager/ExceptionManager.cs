6675b79875dcf4d95ec576f6bbec09a190edcaf7
Oleksii Lytvynov <Oleksii_Lytvynov@epam.com>
2021-02-03 13:03:11 +0200

    Added basic functionality, removed unused project

---
+++ QaLabs/ExceptionManager/ExceptionManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionManager
+{
+    public class ExceptionManager
+    {
+        public int CriticalExceptionCount { get; private set; }
+
+        public int ExceptionCount { get; private set; }
+
+        private readonly List<Type> _criticalExceptionTypes;
+
+        public ExceptionManager()
+        {
+            _criticalExceptionTypes = new List<Type>()
+            {
+                typeof(IndexOutOfRangeException),
+                typeof(NullReferenceException),
+                typeof(InvalidCastException),
+            };
+        }
+
+        public bool IsCriticalException(Exception exception)
+        {
+            return _criticalExceptionTypes.Contains(exception.GetType());
+        }
+
+        public void HandleException(Exception exception)
+        {
+            if(IsCriticalException(exception))
+            {
+                CriticalExceptionCount++;
+            }
+            else
+            {
+                ExceptionCount++;
+            }
+        }
+    }
+}
