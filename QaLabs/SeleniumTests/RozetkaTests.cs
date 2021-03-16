408c4a62fe1bb87c206ce154cabd63f4444dae6c
Oleksii Lytvynov <Oleksii_Lytvynov@epam.com>
2021-03-16 23:49:21 +0200

    Added Rozetka tests

--- QaLabs/SeleniumTests/RozetkaTests.cs
+++ QaLabs/SeleniumTests/RozetkaTests.cs
@@ -8,53 +8,52 @@ using FluentAssertions;
 
 namespace SeleniumTests
 {
-    public class Tests
+    [TestFixture]
+    public class RozetkaUiTests
     {
-        [TestFixture]
-        public class Chrome_Sample_test
+        private IWebDriver driver;
+        private string homeURL;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetupTest()
         {
-            private IWebDriver driver;
-            public string homeURL;
+            homeURL = "https://rozetka.com.ua/ua/";
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver,
+                System.TimeSpan.FromSeconds(15));
+        }
 
-            [SetUp]
-            public void SetupTest()
-            {
-                homeURL = "https://rozetka.com.ua/ua/";
-                driver = new ChromeDriver();
-            }
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
+        }
 
-            [TearDown]
-            public void TearDownTest()
-            {
-                driver.Close();
-            }
+        [TestCase("Дакимакура с Рикардо Милосом")]
+        [TestCase("javascript для детей")]
+        public void SearchAndAddProductToCart(string productQuery)
+        {
+            driver.Navigate().GoToUrl(homeURL);
+            wait.Until(driver =>
+                driver.FindElement(By.ClassName("search-form__submit")));
+            var searchBar =
+                driver.FindElement(By.ClassName("search-form__input"));
+            searchBar.SendKeys(productQuery);
+            var searchButton = driver.FindElement(By.ClassName("search-form__submit"));
+            searchButton.Click();
+            wait.Until(driver => driver.FindElement(By.ClassName("rz-search-result-qnty")));
+            var resultsQuantity = driver.FindElement(By.ClassName("rz-search-result-qnty"));
+            resultsQuantity.Text.Should().MatchRegex(@"Знайдено\s?\w*? \d* товар\w*");
 
-            [TestCase("Дакимакура с Рикардо Милосом")]
-            [TestCase("javascript для детей")]
-            public void SearchAndAddProductToCart(string productQuery)
-            {
-                driver.Navigate().GoToUrl(homeURL);
-                WebDriverWait wait = new WebDriverWait(driver,
-                    System.TimeSpan.FromSeconds(15));
-                wait.Until(driver =>
-                    driver.FindElement(By.ClassName("search-form__submit")));
-                var searchBar =
-                    driver.FindElement(By.ClassName("search-form__input"));
-                searchBar.SendKeys(productQuery);
-                var searchButton = driver.FindElement(By.ClassName("search-form__submit"));
-                searchButton.Click();
-                wait.Until(driver => driver.FindElement(By.ClassName("rz-search-result-qnty")));
-                var resultsQuantity = driver.FindElement(By.ClassName("rz-search-result-qnty"));
-                resultsQuantity.Text.Should().MatchRegex(@"Знайдено\s?\w*? \d* товар\w*");
+            var buyProductButton = driver.FindElements(By.ClassName("goods-tile__buy-button")).First();
+            buyProductButton.Click();
+            var productName = driver.FindElements(By.ClassName("goods-tile__title")).First().Text;
 
-                var buyProductButton = driver.FindElements(By.ClassName("goods-tile__buy-button")).First();
-                buyProductButton.Click();
-                var productName = driver.FindElements(By.ClassName("goods-tile__title")).First().Text;
-
-                driver.FindElement(By.XPath("//button[@opencart]")).Click();
-                var productTitle = driver.FindElement(By.ClassName("cart-product__title"));
-                productTitle.Text.Should().Contain(productName);
-            }
+            driver.FindElement(By.XPath("//button[@opencart]")).Click();
+            wait.Until(driver => driver.FindElement(By.ClassName("modal__heading")));
+            var productTitle = driver.FindElement(By.ClassName("cart-product__title"));
+            productTitle.Text.Should().Contain(productName);
         }
     }
 }
